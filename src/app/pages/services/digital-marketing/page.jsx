93c4855c12eb515a731af420352b23633b2a9cd9
DevPradeep23 <148309945+DevPradeep23@users.noreply.github.com>
2023-10-18 21:33:53 +0530

    Update page.jsx

--- src/app/pages/services/digital-marketing/page.jsx
+++ src/app/pages/services/digital-marketing/page.jsx
@@ -13,7 +13,7 @@ const page = () => {
                     <div className='md:mt-24 mt-8'>
 
                         <div className='md:ml-10'>
-                            <div className='mt-2'>
+                            <div className='mt-20'>
                                 <h1 className='font-semibold text-transparent bg-clip-text bg-gradient-to-r from-[#152C4E] to-[#75EAE4]'>
                                     {"Digital Marketing"}<br />{"Solutions"}
                                 </h1>
