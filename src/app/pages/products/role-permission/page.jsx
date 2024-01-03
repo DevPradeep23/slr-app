f74b8d10bb6b478d49de1e354cb134ca586b6f59
DevPradeep23 <148309945+DevPradeep23@users.noreply.github.com>
2024-01-03 10:13:10 +0530

    Add files via upload

--- src/app/pages/products/role-permission/page.jsx
+++ src/app/pages/products/role-permission/page.jsx
@@ -1,5 +1,6 @@
 import Contact from '@/components/Contact'
 import Image from 'next/image'
+import Link from 'next/link'
 import { BsPlusCircleFill } from 'react-icons/bs'
 import { FaCheck } from 'react-icons/fa'
 
@@ -18,19 +19,22 @@ const page = () => {
 
       <div id='home' className={`md:h-screen md:bg-role_permission bg-role_permission_mobile backgroundPosition md:w-full h-[250px] bg-cover bg-center bg-no-repeat`} >
         <div className='md:max-w-[1240px] w-full mx-auto md:p-16 p-4 flex justify-start items-center'>
-          <div className='md:mt-24 mt-8'>
+          <div className='md:mt-24 mt-10'>
 
             <div className='md:ml-10'>
-              <div className='mt-2'>
-                <h1 className='font-semibold md:text-6xl text-2xl md:mb-2 mt-4 text-transparent bg-clip-text bg-gradient-to-r from-[#152C4E] to-[#75EAE4]'>
+              <div className='md:mb-10 mb-2'>
+                <h1 className='font-semibold md:text-6xl text-xl md:mb-2 mt-2 leading-none text-transparent bg-clip-text bg-gradient-to-r from-[#152C4E] to-[#75EAE4]'>
                   {"Roles &"}<br />{"Permissions"}
                 </h1>
-                <p className='md:max-w-[620px] text-[#202124] m-auto font-inter font-semibold md:text-[14px] text-[12px]'>
+                <p className='md:block hidden md:max-w-[620px] text-[#202124] m-auto font-inter md:font-semibold md:text-[14px] text-[12px]'>
                   {"Define access control for various administrative roles, create custom roles with the respective permissions & delegate responsibilities accordingly."}
+                </p>
+                <p className='md:hidden block md:max-w-[620px] text-[#202124] m-auto font-inter md:font-semibold md:text-[14px] text-[12px] mt-1'>
+                  {"Define access control"} <br /> {"for various administrative"} <br /> {"roles, create custom roles with the respective permissions &"} <br /> {"delegate responsibilities accordingly."}
                 </p>
               </div>
 
-              <button type="button" className="md:ml-1 md:mt-6 mt-1 rounded-full hover:scale-105 ease-in duration-100 text-white items-start bg-[#152C4E] focus:ring-4 font-medium mb-2 capitalize dark:focus:ring-[#152C4E] md:px-6 md:py-2 px-4 py-2 md:text-[14px] text-[10px]">{"GET STARTED ->"} </button>
+              <Link href="/pages/get-in-touch" className="md:ml-1 rounded-full hover:scale-105 ease-in duration-100 text-white items-start bg-[#152C4E] focus:ring-4 font-medium mb-2 capitalize dark:focus:ring-[#152C4E] md:px-6 md:py-2 px-4 py-2 md:text-[14px] text-[10px]">{"GET STARTED ->"} </Link>
 
             </div>
 
