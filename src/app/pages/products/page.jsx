dd873aadfb555ef633a33b28496f9d54f923b7b2
DevPradeep23 <148309945+DevPradeep23@users.noreply.github.com>
2024-01-03 10:04:06 +0530

    Add files via upload

--- src/app/pages/products/page.jsx
+++ src/app/pages/products/page.jsx
@@ -1,6 +1,7 @@
 import Contact from '@/components/Contact'
 import SapUi from '@/components/SapUi'
 import Image from 'next/image'
+import Link from 'next/link'
 import { BsPlusCircleFill } from 'react-icons/bs'
 import { FaCheck } from 'react-icons/fa'
 
@@ -27,18 +28,23 @@ const page = () => {
     <>
 
       <div id='home' className={`md:h-screen md:bg-product_banner bg-product_banner_mobile backgroundPosition md:w-full h-[250px] bg-cover bg-center bg-no-repeat`} >
-        <div className='md:max-w-[1240px] w-full mx-auto md:p-16 p-6 flex justify-start items-center'>
+        <div className='md:max-w-[1240px] w-full mx-auto md:p-16 p-4 flex justify-start items-center'>
           <div className='md:mt-24 mt-4'>
             <div className='md:ml-10'>
-              <div className=''>
-                <h1 className='font-semibold md:text-6xl text-2xl md:mb-2 mt-8 text-transparent bg-clip-text bg-gradient-to-r from-[#152C4E] to-[#75EAE4]'>
+              <div className='md:mb-10 mb-4'>
+                <h1 className='font-semibold md:text-6xl leading-none text-xl md:mb-2 mt-10 md:mt-8 text-transparent bg-clip-text bg-gradient-to-r from-[#152C4E] to-[#75EAE4]'>
                   {"Empowering"}<br />{"Businesses with SAP"}
                 </h1>
-                <p className='md:max-w-[525px] text-[#202124] m-auto font-inter font-semibold md:text-[14px] text-[12px]'>
+                <p className='md:block hidden md:max-w-[525px] text-[#202124] m-auto font-inter md:font-semibold md:text-[14px] text-[12px]'>
                   {"Explore our SAP solutions perfectly crafted to boost efficiency, foster innovation, and drive business growth."}
                 </p>
+                <p className='md:hidden block md:max-w-[525px] mt-2 text-[#202124] m-auto font-inter md:font-semibold md:text-[14px] text-[12px]'>
+                  {"Explore our SAP solutions"} <br /> {"perfectly crafted to boost efficiency,"} <br /> {"foster innovation, and drive business growth."}
+                </p>
               </div>
-              <button type="button" className="md:ml-1 md:mt-6 mt-1 rounded-full hover:scale-105 ease-in duration-100 text-white items-start bg-[#152C4E] focus:ring-4 font-medium mb-2 capitalize dark:focus:ring-[#152C4E] md:px-6 md:py-2 px-4 py-2 md:text-[14px] text-[10px]">{"HIRE US ->"} </button>
+
+              <Link href="/pages/get-in-touch" className="md:ml-1 mt-4 rounded-full hover:scale-105 ease-in duration-100 text-white items-start bg-[#152C4E] focus:ring-4 font-medium mb-2 capitalize dark:focus:ring-[#152C4E] md:px-6 md:py-2 px-4 py-2 md:text-[14px] text-[10px]">{"HIRE US ->"} </Link>
+
             </div>
           </div>
         </div>
